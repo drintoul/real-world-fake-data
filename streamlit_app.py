c4b36747281e51afdcb96bf19b2daa2a82b68b7a
Dave Rintoul <drintoul@gmail.com>
2024-01-22 17:02:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ st.subheader('Generate Fake data in CSV')
 
 def main():
 
-	rows = st.number_input('Enter number of rows desired', min_value=1, max_value=25, step=1)
+	rows = st.number_input('Enter number of rows desired', min_value=1, max_value=25, value="min", step=1)
 
 	fake = Faker()
 
