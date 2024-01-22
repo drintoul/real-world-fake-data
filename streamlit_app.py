292475557e10e73ffd70bab2046db5430d9994e1
Dave Rintoul <drintoul@gmail.com>
2024-01-22 17:11:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,14 @@ def main():
 
 	fake = Faker()
 
+	df = pd.DataFrame()
+	df.columns = ['Name', 'Address', 'Birthdate']
+
 	if rows > 1:
 		for _ in range(rows):
-			st.write(fake.name(), '\t', fake.address(), '\t', fake.date())
+			df = df.concat([df, pd.DataFrame((fake.name(), fake.address(), fake.date())
+
+	st.dataframe(df)
 
 if __name__ == '__main__':
 	
