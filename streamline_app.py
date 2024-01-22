669bb4d0fa57219431e4a12919ee6bd05c571114
Dave Rintoul <drintoul@gmail.com>
2024-01-22 16:53:58 -0500

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -1,31 +1,18 @@
 import streamlit as st
+from faker import Faker
 import pandas as pd
 
-st.set_page_config(page_title="Canada/US Gas Price Comparison")
-st.title('Canada/US Gas Price Comparison')
-st.subheader('Calculate gas prices including currency exchange')
+st.set_page_config(page_title="Real World Fake Data")
+st.title('Real World Fake Data')
+st.subheader('Generate Fake data in CSV')
 
 def main():
 
-	@st.cache_data(ttl=3600*6)
-	def fetch_exchange_rate():
+	fake = Faker()
 
-		dfs = pd.read_html("https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates")
-		df = dfs[0]
-		df = df[df['Currency'] == 'US dollar']
-		asof = df.columns[-1]
-		return df, asof
+	for _ in range(10):
+		st.write(fake.name())
 
-	df, asof = fetch_exchange_rate()
-
-	cdn = st.toggle("Start with Canadian Dollars", value=True)
-
-	usd2cdn = df.iloc[-1, -1]
-	cdn2usd = round(1 / usd2cdn, 4)
-
-	st.write("""Exchange Rate from Bank of Canada:
-	https://www.bankofcanada.ca/rates/exchange/daily-exchange-rates""")
-
-if ____ == 'main':
-
-  main()
+if __name__ == '__main__':
+	
+	main()	
