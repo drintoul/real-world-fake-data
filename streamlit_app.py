d9a0559015301b19e58f1386f2e5d2d7ee58e8c1
Dave Rintoul <drintoul@gmail.com>
2024-01-22 20:49:47 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,16 @@ import numpy as np
 st.set_page_config(page_title="Real World Fake Data")
 st.title('Real World Fake Data')
 
+def specify_dims():
+
+	col1, col2 = st.columns(2)
+	with col1:
+		columns = st.slider('Columns', min_value=1, max_value=10, step=1, value=1)
+	with col2:
+		rows = st.select_slider('Rows', options=[1, 10, 100, 1000], value=1)
+
+	return columns, rows
+
 def show_grid(columns, rows):
 
 	names = []
@@ -24,11 +34,7 @@ def show_grid(columns, rows):
 
 def main():
 	
-	col1, col2 = st.columns(2)
-	with col1:
-		columns = st.slider('Columns', min_value=1, max_value=10, step=1, value=1)
-	with col2:
-		rows = st.select_slider('Rows', options=[1, 10, 100, 1000], value=1)
+	columns, rows = specify_dims()
 
 	show_grid(columns, rows)
 
