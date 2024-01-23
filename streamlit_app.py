3f28b529bfe8b5f34f8302e32b3fa474a24b0304
Dave Rintoul <drintoul@gmail.com>
2024-01-22 20:29:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,11 @@ def main():
 	col1, col2 = st.columns(2)
 
 	with col1:
-		columns = st.select_slider('Columns', 1, 10, 1)
+		st.write('Columns')
+		columns = st.select('Columns', min_value=1, max_value=10, step=1, value=1)
 	with col2:
-		rows = st.select_slider('Rows', options=[1, 10, 100, 1000])
+		st.write('Rows')
+		rows = st.select_slider('Rows', options=[1, 10, 100, 1000], value=1)
 
 	st.write(columns, rows)
 
