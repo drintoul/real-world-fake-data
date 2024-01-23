83f23072a8c6c170db178c68878358c2746763a3
Dave Rintoul <drintoul@gmail.com>
2024-01-22 20:42:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,9 @@ def show_grid(columns, rows):
 		
 		col1, col2 = st.columns(2)
 		with col1:
-			name[col] = st.text_input(value="")
+			name[col] = st.text_input("")
 		with col2:
-			type[col] = st.selectbox(options=['Name', 'Address', 'SSN'])
+			type[col] = st.selectbox("", options=['Name', 'Address', 'SSN'])
 	st.write(name)
 	st.write(type)
 
